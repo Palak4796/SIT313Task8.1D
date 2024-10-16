60d3ff00d0bffc425ad222ec1ea3a1312dd60d87
Palak4796 <palak4796.be23@chitkara.edu.in>
2024-10-16 15:42:34 +0530

    SIT313

---
+++ application/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+// HomePage.js
+import React from 'react';
+import './HomePage.css'; 
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1 className="welcome-header">Welcome to DEV@Deakin</h1>
+    </div>
+  );
+};
+
+export default HomePage;
