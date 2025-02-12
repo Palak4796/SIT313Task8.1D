d8220e15c18338a53619ee4652d7b182b571bc08
Palak4796 <palak4796.be23@chitkara.edu.in>
2025-02-12 23:00:03 +0530

    Update test.js

--- application/src/tests/test.js
+++ application/src/tests/test.js
@@ -1,8 +1,10 @@
 import 'whatwg-fetch'; // Polyfill for fetch API
-import 'text-encoding'; // Polyfill for TextEncoder and TextDecoder
+import { TextEncoder, TextDecoder } from 'text-encoding';
 
 import { render, screen } from '@testing-library/react';
 import App from '../App';  // Adjust the import based on your file structure
+
+
 
 // Make sure global objects are available in your test environment
 global.TextEncoder = require('text-encoding').TextEncoder;
