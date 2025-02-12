ba5be04620a06c61a9d6429329815994309f0eb7
Palak4796 <palak4796.be23@chitkara.edu.in>
2025-02-12 23:11:12 +0530

    Update test.js

--- application/src/tests/test.js
+++ application/src/tests/test.js
@@ -1,16 +1,12 @@
-import 'whatwg-fetch'; // Polyfill for fetch API
 import { render, screen } from '@testing-library/react';
-import App from '../App';  // Adjust the import based on your file structure
-
-// Polyfill TextEncoder and TextDecoder globally
-global.TextEncoder = require('util').TextEncoder;
-global.TextDecoder = require('util').TextDecoder;
+import App from '../App.js';  // Adjust the import based on your file structure
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
 
 
 
