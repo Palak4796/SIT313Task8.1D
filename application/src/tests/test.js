bf2b626fb7e3fb5fbe6ddb28a511cabad9112cfe
Palak4796 <palak4796.be23@chitkara.edu.in>
2025-02-12 22:52:06 +0530

    Update test.js

--- application/src/tests/test.js
+++ application/src/tests/test.js
@@ -5,6 +5,9 @@ import 'text-encoding';
 import { render, screen } from '@testing-library/react';
 import App from '../App.js';  // Adjust the import based on your file structure
 
+global.TextEncoder = require('text-encoding').TextEncoder;
+global.TextDecoder = require('text-encoding').TextDecoder;
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
