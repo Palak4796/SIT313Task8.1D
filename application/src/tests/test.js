47b221f8f36f0ce349ed46d20733b6f22a6400ec
Palak4796 <palak4796.be23@chitkara.edu.in>
2025-02-12 23:07:55 +0530

    Update test.js

--- application/src/tests/test.js
+++ application/src/tests/test.js
@@ -1,20 +1,16 @@
 import 'whatwg-fetch'; // Polyfill for fetch API
-import { TextEncoder, TextDecoder } from 'text-encoding';
-
 import { render, screen } from '@testing-library/react';
 import App from '../App';  // Adjust the import based on your file structure
 
-global.TextEncoder = require("util").TextEncoder;
-
-
-// Make sure global objects are available in your test environment
-global.TextEncoder = require("util").TextEncoder;
-global.TextDecoder = require("util").TextDecoder;
+// Polyfill TextEncoder and TextDecoder globally
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
 
 
