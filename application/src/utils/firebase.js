80f9b49dedeaec86676e38855a589f4baf6b63ce
Palak4796 <palak4796.be23@chitkara.edu.in>
2025-02-17 00:29:56 +0530

    Update firebase.js

--- application/src/utils/firebase.js
+++ application/src/utils/firebase.js
@@ -1,22 +1,22 @@
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { TextEncoder } from 'util'; // Consistent ES module import
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: ""
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBu-VX3ZZ1O3kDgHr3NKeybFOzzq_7SraI",
+  authDomain: "task81dis.firebaseapp.com",
+  projectId: "task81dis",
+  storageBucket: "task81dis.appspot.com",
+  messagingSenderId: "679940073377",
+  appId: "1:679940073377:web:812218014aea7ca5af8de0"
 };
 
 const app = initializeApp(firebaseConfig); 
 
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 
+
 export { db, storage };
-
-
