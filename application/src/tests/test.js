c32d7548f2fbf4f3a40945e1eccda739c65efeaa
Palak4796 <palak4796.be23@chitkara.edu.in>
2025-02-19 11:55:22 +0530

    Update test.js

--- application/src/tests/test.js
+++ application/src/tests/test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import { TextEncoder, TextDecoder } from 'util';
 import App from '../App.js';  // Adjust the import based on your file structure
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 test('renders learn react link', () => {
   render(<App />);
